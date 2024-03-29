6d12abc44b0c241198299e2ba4ce395d35f57dca
NaoraYuki <naora.yuki@lance-digital.com>
2024-03-29 12:54:21 +0900

    スマホ対応＋デザイン修正

--- app/funclub/page.tsx
+++ app/funclub/page.tsx
@@ -7,12 +7,12 @@ export default function Page() {
     <div className={styles.container}>
       <img className={styles.title} src="ファンクラブ.png" alt="" />
       <img className={styles.fanapplogo} src="aorab-fanapp.png" alt="" />
-      <h1 className={styles.text}>青ラビモバイルとは</h1>
+      <h1 className={styles.text}>◆青ラビモバイルとは</h1>
       <p>
         青ラビモバイルは、青山rabnessのファンコミュニティアプリです。これまでのファンクラブとは異なり、ファンは、運営のサポート等を通じてアイドル運営の一部に携わることもできます。
         メンバー、運営、ファンが一丸となって、日本一愛されるアイドルグループ「青山Rabness」を作りあげましょう！
       </p>
-      <h1 className={styles.text}>アプリのダウンロード</h1>
+      <h1 className={styles.text}>◆アプリのダウンロード</h1>
       <div className={styles.appDownload}>
         <div className={styles.app}>
           <p>android版</p>
@@ -33,7 +33,7 @@ export default function Page() {
         </div>
       </div>
       {/* <img className={styles.functionImg} src="appfunction.png" alt="" /> */}
-      <h1 className={styles.text}>青ラビモバイルの機能</h1>
+      <h1 className={styles.text}>◆青ラビモバイルの機能</h1>
       <ul className={styles.function}>
         <li>
           <h2>タイムライン</h2>
@@ -55,18 +55,18 @@ export default function Page() {
           </p>
         </li>
       </ul>
-      <h1 className={styles.text}>コミュニティへの参加</h1>
+      <h1 className={styles.text}>◆コミュニティへの参加</h1>
       <p>
         青ラビモバイルでは、コミュニティへの参加に際しコミュニティ会員費用(月額)が必要となります。
         コミュニティ会員費用は、コミュニティ毎に異なります。会員費用を確認したい場合は、各コミュニティの詳細画面をご参照下さい。
       </p>
-      <h1 className={styles.text}>機種変更時の復元</h1>
+      <h1 className={styles.text}>◆機種変更時の復元</h1>
       <p>
         機種変更時であっても、以前加入したコミュニティは引き続きご利用になれます。
         コミュニティ加入時と同じアカウントで青ラビモバイルにログインしてください。
         また、購入履歴を復元したい場合は、本画面の「購入を復元する」ボタンを押下してください。
       </p>
-      <h1 className={styles.text}>コミュニティの退会</h1>
+      <h1 className={styles.text}>◆コミュニティの退会</h1>
       <p>
         コミュニティを退会したい場合は、以下の順に操作を行ってください。
         <br />
@@ -76,22 +76,22 @@ export default function Page() {
         <br />
         ③注意事項を確認の上、退会ボタンを選択する
       </p>
-      <h1 className={styles.text}>自動継続課金について</h1>
+      <h1 className={styles.text}>◆自動継続課金について</h1>
       <p>コミュニティの会員権は、コミュニティを退会しない限り自動更新されます。</p>
-      <h1 className={styles.text}>注意点</h1>
+      <h1 className={styles.text}>◆注意点</h1>
       <p>
         コミュニティ会員費用はiTunesアカウントを経由して課金されます。
         <br />
         コミュニティを退会した場合、退会前のアカウントは復旧できません。
       </p>
-      <h1 className={styles.text}>利用規約・プライバシーポリシー</h1>
+      <h1 className={styles.text}>◆利用規約・プライバシーポリシー</h1>
       <p>
         ・利用規約<a href="https://butaiura.fan/lp/terms">https://butaiura.fan/lp/terms</a>
         <br />
         ・プライバシーポリシー
         <a href="https://butaiura.fan/lp/privacy">https://butaiura.fan/lp/privacy</a>
       </p>
-      <h1 className={styles.text}>動作環境</h1>
+      <h1 className={styles.text}>◆動作環境</h1>
       <p>
         iOS 12以上
         <br />
@@ -99,7 +99,7 @@ export default function Page() {
         <br />
         ※快適に動作することを保証するものではありません
       </p>
-      <h1 className={styles.text}>推奨環境</h1>
+      <h1 className={styles.text}>◆推奨環境</h1>
       <p>
         「動作環境」に加えて、以下の条件を満たす環境を推奨しています。
         <br />
